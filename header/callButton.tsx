5bd7af7439eaee7d3f20c302db55e3f8df720722
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-04 11:18:52 -0500

    attempting graphql query

--- header/callButton.tsx
+++ header/callButton.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import { Image } from '../utilities'
 
+type dataObject = {
+  allFile: {
+    nodes: {
+      publicURL: string,
+      relativePath: string,
+      childImageSharpFluid: {
+        fluid: any
+      }
+    }
+  }
+}
+
 export const CallButton = ({ block, styles, images }) => {
+  const [data] = useState<dataObject>(useStaticQuery(graphql`
+  {
+    allFile(filter: {relativeDirectory: {eq: "images"}}) {
+      nodes {
+        publicURL
+        relativePath
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`))
+
+  useEffect(() => {
+    if (data) {
+      console.log(data)
+    }
+  }, [data])
+
   return (
     <a className={styles.callButton} href={`tel:+${block.phoneNumber}`}>
       <Image className={styles.icon} src={block.icon.image} alt={block.icon.alt} title={block.icon.title} images={images} />
