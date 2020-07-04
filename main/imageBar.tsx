de4655d3aa1e395191977bfd1a2434f8b734756b
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-04 16:09:03 -0500

    removed console  logs

--- main/imageBar.tsx
+++ main/imageBar.tsx
@@ -2,20 +2,16 @@ import React from 'react'
 
 import { classNames, enabled, Image } from '../exports'
 
-export const ImageBar = ({ block, styles, images }) => {
-  console.log(block)
-
-  return (
-    <section className={classNames(block, styles, 'section')}>
-      {enabled(block.images).map((item, i) => {
-        return item.url ? (
-          <a className={styles.link} key={i} href={item.url}>
-            {item.image ? <Image className={styles.image} src={item.image} alt={item.alt} title={item.title} images={images} /> : <p>{item.alt}</p>}
-          </a>
-        ) : (
-          item.image ? <Image key={i} className={styles.image} src={item.image} alt={item.alt} title={item.title} images={images} /> : <p>{item.alt}</p>
-        )
-      })}
-    </section>
-  )
-}
+export const ImageBar = ({ block, styles, images }) => (
+  <section className={classNames(block, styles, 'section')}>
+    {enabled(block.images).map((item, i) => {
+      return item.url ? (
+        <a className={styles.link} key={i} href={item.url}>
+          {item.image ? <Image className={styles.image} src={item.image} alt={item.alt} title={item.title} images={images} /> : <p>{item.alt}</p>}
+        </a>
+      ) : (
+        item.image ? <Image key={i} className={styles.image} src={item.image} alt={item.alt} title={item.title} images={images} /> : <p>{item.alt}</p>
+      )
+    })}
+  </section>
+)
