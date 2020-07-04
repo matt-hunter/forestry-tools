100052227d2781a6079e22290a7fee80df8ac748
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-04 13:50:59 -0500

    clean up code

--- utilities/mainBlock.tsx
+++ utilities/mainBlock.tsx
@@ -2,52 +2,38 @@ import React from 'react'
 
 import { ContactForm, ContentColumns, ContentTabs, Hero, ImageBar, Testimonial } from '../main'
 
-export default (block, i, images, styles) => [{
-  name: 'main-contact-form',
-  template: <ContactForm key={i} block={block} styles={styles} />
-}, {
-  name: 'main-filtered-images-slider',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-content-columns',
-  template: <ContentColumns key={i} block={block} styles={styles} images={images} />
-}, {
-  name: 'main-content-tabs',
-  template: <ContentTabs key={i} block={block} styles={styles} images={images} />
-}, {
-  name: 'main-gallery',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-hero',
-  template: <Hero key={i} block={block} styles={styles} images={images} />
-}, {
-  name: 'main-image-bar',
-  template: <ImageBar key={i} block={block} styles={styles} images={images} />
-}, {
-  name: 'main-images',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-knowledge-base',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-map',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-posts',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-recent-research',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-research-grid',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-research-slider',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-slideshow',
-  template: <p key={i}>{block.template}</p>
-}, {
-  name: 'main-testimonial',
-  template: <Testimonial key={i} block={block} styles={styles} images={images} />
-}].find(template => template.name === block.template).template
+export default (block, i, images, styles) => {
+  return block.template === 'main-contact-form' ? (
+    <ContactForm key={i} block={block} styles={styles} />
+  ) : block.template === 'main-filtered-images-slider' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-content-columns' ? (
+    <ContentColumns key={i} block={block} styles={styles} images={images} />
+  ) : block.template === 'main-content-tabs' ? (
+    <ContentTabs key={i} block={block} styles={styles} images={images} />
+  ) : block.template === 'main-gallery' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-hero' ? (
+    <Hero key={i} block={block} styles={styles} images={images} />
+  ) : block.template === 'main-image-bar' ? (
+    <ImageBar key={i} block={block} styles={styles} images={images} />
+  ) : block.template === 'main-images' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-knowledge-base' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-map' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-posts' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-recent-research' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-research-grid' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-research-slider' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-slideshow' ? (
+    <p key={i}>{block.template}</p>
+  ) : block.template === 'main-testimonial' ? (
+    <Testimonial key={i} block={block} styles={styles} images={images} />
+  ) : null
+}
