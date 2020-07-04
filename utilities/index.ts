78c4b63318fbfe64685af11e6b396b0601d89b5b
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-04 13:18:02 -0500

    added mainBlock utility

--- utilities/index.ts
+++ utilities/index.ts
@@ -3,5 +3,6 @@ import enabled from './enabled'
 import getPage from './getPage'
 import Image from './image'
 import link from './link'
+import mainBlock from './mainBlock'
 
-export { classNames, enabled, getPage, Image, link }
+export { classNames, enabled, getPage, Image, link, mainBlock }
