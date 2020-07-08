b1b3283b5a1918f59adf901d763183e5e1ee8670
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-08 14:06:03 -0500

    console log block for easy dev

--- footer/copyrightInfo.tsx
+++ footer/copyrightInfo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export const CopyrightInfo = ({ block, styles, pages }) => {
+  console.log(block.template, block)
   return (
     <section className={styles.default}>
       <p>{block.template}</p>
--- footer/logo.tsx
+++ footer/logo.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export const Logo = ({ block, styles, images }) => {
+  console.log(block.template, block)
   return (
     <section className={styles.default}>
       <p>{block.template}</p>
