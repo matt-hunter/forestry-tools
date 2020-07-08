e5ea50c045b983b6a37a3dcfe05ff34149282bd3
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-08 15:21:19 -0500

    Cities block

--- main/cities.tsx
+++ main/cities.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 
-import { classNames } from '../exports'
+import { classNames, enabled, Image } from '../exports'
 
-export const Cities = ({ block, styles }) => {
-  const blockValues = Object.entries(block).filter(([name, value]) => value !== null)
-    .map(([name, value]) => ({ name, value }))
-  console.log(block.template, blockValues)
+export const Cities = ({ block, styles, images }) => {
   return (
     <section className={classNames(block, styles)}>
-      <p>{block.template}</p>
+      <div className={styles.textContainer}>
+        {block.title && <h1 className={styles.title}>{block.title}</h1>}
+        {block.body && <p className={block.body}>{block.body}</p>}
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Get in touch</button>
+        </div>
+      </div>
+      {enabled(block.cities).map((city, i) => {
+        return (
+          <div key={i} className={styles.city}>
+            <Image className={styles.image} src={city.image.image} images={images} title={city.image.title} alt={city.image.alt} />
+            <p className={styles.name}>{city.name}</p>
+            <p className={styles.nativeLanguageName}>{city.nativeLanguageName}</p>
+          </div>
+        )
+      })}
     </section>
   )
 }
