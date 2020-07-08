a76825e4de09528b1ead2f4ef4940f0ac2f7e43e
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-08 18:09:05 -0500

    footer logo

--- footer/logo.tsx
+++ footer/logo.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
+import { Image } from '../exports'
+
 export const Logo = ({ block, styles, images }) => {
-  const blockValues = Object.entries(block).filter(([name, value]) => value !== null)
-    .map(([name, value]) => ({ name, value }))
-  console.log(block.template, blockValues)
   return (
     <section className={styles.default}>
-      <p>{block.template}</p>
+      <Image images={images} className={styles.image} src={block.image} title={block.title} alt={block.alt} />
     </section>
   )
 }
