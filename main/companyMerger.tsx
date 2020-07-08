2985db7fb5987d936ffbdfae315b4780c0a34aab
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-08 15:42:48 -0500

    CompanyMerger

--- main/companyMerger.tsx
+++ main/companyMerger.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
-import { classNames } from '../exports'
+import { classNames, enabled, Image } from '../exports'
 
-export const CompanyMerger = ({ block, styles }) => {
-  const blockValues = Object.entries(block).filter(([name, value]) => value !== null)
-    .map(([name, value]) => ({ name, value }))
-  console.log(block.template, blockValues)
+export const CompanyMerger = ({ block, styles, images }) => {
   return (
     <section className={classNames(block, styles)}>
-      <p>{block.template}</p>
+      <div className={styles.textContainer}>
+        {block.heading && <h1 className={styles.heading}>{block.heading}</h1>}
+        {block.body && <p className={styles.body}>{block.body}</p>}
+      </div>
+      {enabled(block.cities).map((city, i) => {
+        return (
+          <div className={styles.city} key={i}>
+            {i > 0 && <p>+</p>}
+            <Image className={styles.image} images={images} src={city.logo.image} title={city.logo.title} alt={city.logo.alt} />
+            {city.description && <p className={styles.description}>{city.description}</p>}
+          </div>
+        )
+      })}
     </section>
   )
 }
