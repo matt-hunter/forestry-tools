bdbb3ec67b454dc36d39df9ba0c7132e63482953
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-07 23:48:13 -0500

    fixes for videocontactform

--- header/contactFormWithVideoBackground.tsx
+++ header/contactFormWithVideoBackground.tsx
@@ -5,26 +5,28 @@ import ReactPlayer from 'react-player'
 
 export const ContactFormWithVideoBackground = ({ block, images, styles }) => {
   return (
-    <ReactPlayer className={styles.default} video={block.video}>
-      <Image images={images} className={styles.image} src={block.image} container='div'>
-        <form className={styles.form}>
-          <h1 className={styles.heading}>{block.heading}</h1>
-          <h2 className={styles.subheading}>{block.subheading}</h2>
-          <fieldset className={styles.nameSet}>
-            <label className={styles.nameLabel}>{block.nameLabel}</label>
-            <input className={block.name} type='text' />
-          </fieldset>
-          <fieldset className={styles.method}>
-            <label className={styles.methodLabel}>{block.phoneLabel}</label>
-            <input className={styles.method} type='text' />
-          </fieldset>
-          <fieldset className={styles.message}>
-            <label className={styles.messageLabel}>{block.messageLabel}</label>
-            <textarea className={styles.message} />
-          </fieldset>
-          <button className={styles.submit}>Send</button>
-        </form>
-      </Image>
-    </ReactPlayer>
+    <div className={styles.default}>
+      <div className={styles.videoContainer}>
+        <ReactPlayer playing muted className={styles.video} url={block.video} width='100%' height='100%' />
+      </div>
+      <Image images={images} className={styles.image} src={block.image} />
+      <form className={styles.form}>
+        <h1 className={styles.heading}>{block.heading}</h1>
+        <h2 className={styles.subheading}>{block.subheading}</h2>
+        <fieldset className={styles.nameSet}>
+          <label className={styles.nameLabel}>{block.nameLabel}</label>
+          <input className={styles.name} type='text' />
+        </fieldset>
+        <fieldset className={styles.methodSet}>
+          <label className={styles.methodLabel}>{block.phoneLabel}</label>
+          <input className={styles.method} type='text' />
+        </fieldset>
+        <fieldset className={styles.messageSet}>
+          <label className={styles.messageLabel}>{block.messageLabel}</label>
+          <textarea className={styles.message} />
+        </fieldset>
+        <button className={styles.submit}>Send</button>
+      </form>
+    </div>
   )
 }
