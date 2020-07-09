1a3a5a8a335f23bb07c4920d0517047516058822
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-08 20:16:49 -0500

    animated Images fix

--- main/animatedImage.tsx
+++ main/animatedImage.tsx
@@ -8,19 +8,14 @@ export const AnimatedImage = ({ block, styles, images }) => {
     <section className={classNames(block, styles)}>
       {enabled(block.backgrounds).map((background, i) => {
         return (
-          <Image key={i} className={styles.image} images={images} src={background.image} container='div' title={background.title} alt={background.alt}>
-            {i === block.backgrounds.length - 1 && (
-              <>
-                <div className={styles.textContainer}>
-                  <h1 className={styles.heading1}>{block.heading1}</h1>
-                  <h2 className={styles.heading2}>{block.heading2}</h2>
-                </div>
-                <div className={styles.animatedElement + ' ' + styles[camelcase(block.animatedElement)]} />
-              </>
-            )}
-          </Image>
+          <Image key={i} className={styles.image} images={images} src={background.image} title={background.title} alt={background.alt} />
         )
       })}
+      <div className={styles.textContainer}>
+        <h1 className={styles.heading1}>{block.heading1}</h1>
+        <h2 className={styles.heading2}>{block.heading2}</h2>
+      </div>
+      <div className={styles.animatedElement + ' ' + styles[camelcase(block.animatedElement)]} />
     </section>
   )
 }
