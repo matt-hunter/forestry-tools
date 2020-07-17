221ff86ee9ad4d7193df9de410a6ae5be8d90bd4
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-17 10:27:33 -0500

    break lines

--- main/cities.tsx
+++ main/cities.tsx
@@ -16,7 +16,6 @@ export const Cities = ({ block, styles, images }) => {
         <div className={styles.featuredCity}>
           <Image className={styles.image} src={enabled(block.cities)[0].image.image} images={images} title={enabled(block.cities)[0].image.title} alt={enabled(block.cities)[0].image.alt} container='div'>
             <p className={styles.name}>{enabled(block.cities)[0].name}</p>
-            <p className={styles.nativeLanguageName}>{enabled(block.cities)[0].nativeLanguageName}</p>
           </Image>
         </div>
       )}
@@ -26,8 +25,8 @@ export const Cities = ({ block, styles, images }) => {
             return i > 0 && (
               <div key={i} className={styles.city}>
                 <Image className={styles.image} src={city.image.image} images={images} title={city.image.title} alt={city.image.alt} container='div'>
-                  <p className={styles.name}>{city.name}</p>
-                  <p className={styles.nativeLanguageName}>{city.nativeLanguageName}</p>
+                  <p className={styles.name} dangerouslySetInnerHTML={{ __html: city.name.replace(' ', '<br />') }} />
+                  <p className={styles.nativeLanguageName} dangerouslySetInnerHTML={{ __html: city.nativeLanguageName.replace(' ', '<br />') }} />
                 </Image>
               </div>
             )
