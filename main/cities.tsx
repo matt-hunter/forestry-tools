56c31a92988d4a712f0bf8c5c2127f46d307376c
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-17 09:54:23 -0500

    Changed layout of cities

--- main/cities.tsx
+++ main/cities.tsx
@@ -12,16 +12,28 @@ export const Cities = ({ block, styles, images }) => {
           <button className={styles.button}>Get in touch</button>
         </div>
       </div>
-      {enabled(block.cities).map((city, i) => {
-        return (
-          <div key={i} className={styles.city}>
-            <Image className={styles.image} src={city.image.image} images={images} title={city.image.title} alt={city.image.alt} container='div'>
-              <p className={styles.name}>{city.name}</p>
-              <p className={styles.nativeLanguageName}>{city.nativeLanguageName}</p>
-            </Image>
-          </div>
-        )
-      })}
+      {enabled(block.cities)[0] && (
+        <div className={styles.featuredCity}>
+          <Image className={styles.image} src={enabled(block.cities)[0].image.image} images={images} title={enabled(block.cities)[0].image.title} alt={enabled(block.cities)[0].image.alt} container='div'>
+            <p className={styles.name}>{enabled(block.cities)[0].name}</p>
+            <p className={styles.nativeLanguageName}>{enabled(block.cities)[0].nativeLanguageName}</p>
+          </Image>
+        </div>
+      )}
+      {enabled(block.cities).length > 1 && (
+        <div className={styles.cities}>
+          {enabled(block.cities).map((city, i) => {
+            return i > 0 && (
+              <div key={i} className={styles.city}>
+                <Image className={styles.image} src={city.image.image} images={images} title={city.image.title} alt={city.image.alt} container='div'>
+                  <p className={styles.name}>{city.name}</p>
+                  <p className={styles.nativeLanguageName}>{city.nativeLanguageName}</p>
+                </Image>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </section>
   )
 }
