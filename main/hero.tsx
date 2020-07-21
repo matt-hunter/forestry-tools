b818c40eaff4cac157c49fe3c827c875b8e4a02f
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-21 11:06:51 -0500

    created component from oilPriceTicker

--- main/hero.tsx
+++ main/hero.tsx
@@ -1,15 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { classNames, enabled, Image } from '../exports'
 
-import arrowDown from '../images/arrow-down.svg'
-import arrowUp from '../images/arrow-up.svg'
-import droplet from '../images/droplet.svg'
+import { OilPriceTicker } from './'
 
 export const Hero = ({ block, styles, images }) => {
-  const [oilData, setOilData] = useState([])
-  const [oilPrices, setOilPrices] = useState([])
-
   const scrollDown = () => {
     document.getElementById('main').scroll({
       left: 0,
@@ -17,38 +12,6 @@ export const Hero = ({ block, styles, images }) => {
       behavior: 'smooth'
     })
   }
-
-  useEffect(() => {
-    window.fetch('http://api.eia.gov/series/?api_key=2c64d808bf1a095b0a22eb601b723c20&series_id=PET.RWTC.D;PET.RBRTE.D').then(async res => {
-      const data = await res.json()
-      setOilData(data.series)
-    // setWti(data.series[0].data)
-    // setBrent(data.series[1].data)
-    })
-  }, [])
-
-  useEffect(() => {
-    if (oilData.length) {
-      const calculateObject = (name, series) => {
-        const price = series.data[0][1]
-        const previous = series.data[1][1]
-        const difference = price - previous
-        const change = (((series.data[0][1] / series.data[1][1]) - 1) * 100).toFixed(2) + '%'
-        return ({
-          name,
-          decrease: difference < 0,
-          price,
-          difference: (difference < 0 ? '' : '+') + difference.toFixed(2),
-          change: (difference < 0 ? '' : '+') + change
-        })
-      }
-
-      setOilPrices([
-        calculateObject('WTI Crude Oil', oilData[0]),
-        calculateObject('Brent Crude Oil', oilData[1])
-      ])
-    }
-  }, [oilData])
 
   return (
     <section id='hero' className={classNames(block, styles)}>
@@ -59,28 +22,7 @@ export const Hero = ({ block, styles, images }) => {
           </div>
         ))}
       </div>
-      <div className={styles.priceContainer}>
-        {oilPrices.map((oilPrice, i) => {
-          return (
-            <div key={i} className={styles.oilPrice}>
-              <span className={styles.name}>{oilPrice.name}</span>
-              <span className={styles.iconsContainer}>
-                <img className={styles.icon} src={droplet} alt='oil' />
-                <span className={styles.change + `${oilPrice.decrease ? ` ${styles.decrease}` : ` ${styles.increase}`}`}>
-                  {oilPrice.decrease ? (
-                    <img className={styles.icon} src={arrowDown} alt='down' />
-                  ) : (
-                    <img className={styles.icon} src={arrowUp} alt='up' />
-                  )}
-                </span>
-              </span>
-              <span className={styles.price}>{oilPrice.price}</span>
-              <span className={styles.difference}>{oilPrice.difference}</span>
-              <span className={styles.percent + `${oilPrice.decrease ? ` ${styles.decrease}` : ` ${styles.increase}`}`}>{oilPrice.change}</span>
-            </div>
-          )
-        })}
-      </div>
+      <OilPriceTicker styles={styles} />
       <div className={styles.textContainer}>
         <h1 className={styles.smallHeading}>{block.smallHeading}</h1>
         <h2 className={styles.largeHeading}>{block.largeHeading}</h2>
--- main/index.ts
+++ main/index.ts
@@ -17,12 +17,13 @@ import { Images } from './images'
 import { KnowledgeBase } from './knowledgeBase'
 import { Leadership } from './leadership'
 import { Map } from './map'
+import { OilPriceTicker } from './oilPriceTicker'
 import { Posts } from './posts'
 import { RecentArticles } from './recentArticles'
 import { Slideshow } from './slideshow'
 import { StaticImage } from './staticImage'
 import { Testimonial } from './testimonial'
 
-export { AnimatedImage, ArticlesGrid, ArticlesSlider, Cities, CompanyMerger, ContactForm, ContentColumns, ContentTabs, FeaturedArticles, FilteredImagesSlider, Gallery, Hero, HeroSlider, Highlights, ImageBar, Images, KnowledgeBase, Leadership, Map, Posts, RecentArticles, Slideshow, StaticImage, Testimonial }
+export { AnimatedImage, ArticlesGrid, ArticlesSlider, Cities, CompanyMerger, ContactForm, ContentColumns, ContentTabs, FeaturedArticles, FilteredImagesSlider, Gallery, Hero, HeroSlider, Highlights, ImageBar, Images, KnowledgeBase, Leadership, Map, OilPriceTicker, Posts, RecentArticles, Slideshow, StaticImage, Testimonial }
 
 export default { AnimatedImage, ArticlesGrid, ArticlesSlider, Cities, CompanyMerger, ContactForm, ContentColumns, ContentTabs, FeaturedArticles, FilteredImagesSlider, Gallery, Hero, HeroSlider, Highlights, ImageBar, Images, KnowledgeBase, Leadership, Map, Posts, RecentArticles, Slideshow, StaticImage, Testimonial }
