626d285aa5cf7652e80ea48a58eb8e479dfc2517
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-20 23:20:20 -0500

    fixes for press page

--- main/featuredArticles.tsx
+++ main/featuredArticles.tsx
@@ -16,6 +16,13 @@ export const FeaturedArticles = ({ block, styles, articles, pages, images }) => 
       path: page.filePath + '/' + article.fields.slug
     })
   })
+  const scrollDown = () => {
+    document.getElementById('main').scroll({
+      left: 0,
+      top: window.innerHeight,
+      behavior: 'smooth'
+    })
+  }
 
   return (
     <section className={classNames(block, styles)}>
@@ -23,7 +30,7 @@ export const FeaturedArticles = ({ block, styles, articles, pages, images }) => 
       <div className={styles.sidebar}>
         <h2 className={styles.subtitle}>{block.subtitle}</h2>
         <p className={styles.body}>{block.body}</p>
-        <Button styles={styles} text={block.button.label} direction={block.button.direction} />
+        <Button onClick={scrollDown} styles={styles} text={block.button.label} direction={block.button.direction} />
       </div>
       <div className={styles.articles}>
         {featured.map((article, i) => {
@@ -32,6 +39,7 @@ export const FeaturedArticles = ({ block, styles, articles, pages, images }) => 
               <Image images={images} className={styles.article} container='div' src={article.image}>
                 <h3 className={styles.title}>{article.title}</h3>
                 <p className={styles.excerpt}>{article.excerpt}</p>
+                <div className={styles.overlay} />
               </Image>
             </Link>
           )
--- shared/button.tsx
+++ shared/button.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import rightArrow from '../images/arrow-right-circle.svg'
 import downArrow from '../images/arrow-down-circle.svg'
 
-export const Button = ({ styles, text, direction }) => {
+export const Button = ({ styles, text, direction, onClick }) => {
   return (
     <div className={styles.buttonContainer}>
-      <button className={styles.button}>
+      <button onClick={onClick} className={styles.button}>
         {direction === 'right' && (
           <img className={styles.arrow} src={rightArrow} alt='right arrow' />
         )}
