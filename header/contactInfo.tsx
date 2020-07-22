3e6e4ea334c379e8d41f000cf08f9f3c13d3607a
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-22 13:59:30 -0500

    put red bar in correct block

--- header/contactInfo.tsx
+++ header/contactInfo.tsx
@@ -3,17 +3,20 @@ import React from 'react'
 export const ContactInfo = ({ styles, contactInfo }) => {
   return (
     <section className={styles.default}>
-      {contactInfo.phoneNumber && (
-        <p className={styles.phoneNumber}>
-          {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
-        </p>
-      )}
-      {contactInfo.email && (
-        <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
-      )}
-      {contactInfo.address && (
-        <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
-      )}
+      <div className={styles.redBar} />
+      <div className={styles.contactInfo}>
+        {contactInfo.phoneNumber && (
+          <p className={styles.phoneNumber}>
+            {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
+          </p>
+        )}
+        {contactInfo.email && (
+          <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
+        )}
+        {contactInfo.address && (
+          <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
+        )}
+      </div>
     </section>
   )
 }
