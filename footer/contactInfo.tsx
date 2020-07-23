4b765f6ad4787f2f1c8735366f2c9db4c1041ba1
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-23 15:24:17 -0500

    added container around data

--- footer/contactInfo.tsx
+++ footer/contactInfo.tsx
@@ -5,18 +5,20 @@ import { Button } from '../shared'
 export const ContactInfo = ({ styles, contactInfo, toggleForm }) => {
   return (
     <section className={styles.default}>
-      {contactInfo.phoneNumber && (
-        <p className={styles.phoneNumber}>
-          {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
-        </p>
-      )}
-      <Button text='Get in touch' direction='right' styles={styles} onClick={toggleForm} />
-      {contactInfo.email && (
-        <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
-      )}
-      {contactInfo.address && (
-        <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
-      )}
+      <div className={styles.container}>
+        {contactInfo.phoneNumber && (
+          <p className={styles.phoneNumber}>
+            {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
+          </p>
+        )}
+        <Button text='Get in touch' direction='right' styles={styles} onClick={toggleForm} />
+        {contactInfo.email && (
+          <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
+        )}
+        {contactInfo.address && (
+          <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
+        )}
+      </div>
     </section>
   )
 }
