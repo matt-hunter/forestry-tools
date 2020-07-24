8a5e80337fa336bc080d7ba7508f9b7001116957
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-24 16:41:51 -0500

    moved text out of image

--- main/staticImage.tsx
+++ main/staticImage.tsx
@@ -6,10 +6,11 @@ import { Button } from '../shared'
 export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
   return (
     <section className={classNames(block, styles)}>
-      <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt}>
+      <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />
+      <div className={styles.textContainer}>
         <p className={styles.text}>{block.text}</p>
         <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
-      </Image>
+      </div>
     </section>
   )
 }
