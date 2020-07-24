92c9d6a73d1b18b5e5f0518bb05689c139ff237e
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-24 17:52:15 -0500

    changed staticImage fields

--- main/staticImage.tsx
+++ main/staticImage.tsx
@@ -8,8 +8,15 @@ export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
     <section className={classNames(block, styles)}>
       <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />
       <div className={styles.textContainer}>
-        <p className={styles.text}>{block.text}</p>
-        <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
+        {block.heading && (
+          <h1 className={styles.heading}>{block.heading}</h1>
+        )}
+        {block.subheading && (
+          <h2 className={styles.subheading}>{block.subheading}</h2>
+        )}
+        {block.button && block.showButton && (
+          <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
+        )}
       </div>
     </section>
   )
