7a141387500fdeeb8aa14f9e86bdff64e041bbf3
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-26 12:57:37 -0500

    disable video on local  development

--- header/contactFormWithVideoBackground.tsx
+++ header/contactFormWithVideoBackground.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 
 import { Image } from '../exports'
 import { Button } from '../shared'
 
-export const ContactFormWithVideoBackground = ({ block, images, styles }) => (
-  <section className={styles.default}>
-    <div className={styles.videoContainer}>
-      <ReactPlayer playing muted loop className={styles.video} url={block.video} width='100%' height='100%' />
-    </div>
-    <Image images={images} className={styles.image} src={block.image} />
-    <form className={styles.form + ' pageclip-form'} action={block.url}>
-      <h1 className={styles.heading}>{block.heading}</h1>
-      <h2 className={styles.subheading}>{block.subheading}</h2>
-      <fieldset className={styles.nameSet}>
-        <label className={styles.nameLabel}>{block.nameLabel}</label>
-        <input name='name' className={styles.name} type='text' required />
-      </fieldset>
-      <fieldset className={styles.methodSet}>
-        <label className={styles.methodLabel}>{block.phoneLabel}</label>
-        <input type='email' name='email' className={styles.method} required />
-      </fieldset>
-      <fieldset className={styles.messageSet}>
-        <label className={styles.messageLabel}>{block.messageLabel}</label>
-        <textarea name='message' rows={1} className={styles.message} required />
-      </fieldset>
-      <Button text='Send' direction='right' styles={styles} className='pageclip-form__submit' type='submit' />
-    </form>
-  </section>
-)
+export const ContactFormWithVideoBackground = ({ block, images, styles }) => {
+  const [playing, setPlaying] = useState(true)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (window.localStorage.getItem('playing') === 'no') setPlaying(false)
+    }
+  }, [])
+
+  return (
+    <section className={styles.default}>
+      <div className={styles.videoContainer}>
+        <ReactPlayer playing={playing} muted loop className={styles.video} url={block.video} width='100%' height='100%' />
+      </div>
+      <Image images={images} className={styles.image} src={block.image} />
+      <form className={styles.form + ' pageclip-form'} action={block.url}>
+        <h1 className={styles.heading}>{block.heading}</h1>
+        <h2 className={styles.subheading}>{block.subheading}</h2>
+        <fieldset className={styles.nameSet}>
+          <label className={styles.nameLabel}>{block.nameLabel}</label>
+          <input name='name' className={styles.name} type='text' required />
+        </fieldset>
+        <fieldset className={styles.methodSet}>
+          <label className={styles.methodLabel}>{block.phoneLabel}</label>
+          <input type='email' name='email' className={styles.method} required />
+        </fieldset>
+        <fieldset className={styles.messageSet}>
+          <label className={styles.messageLabel}>{block.messageLabel}</label>
+          <textarea name='message' rows={1} className={styles.message} required />
+        </fieldset>
+        <Button text='Send' direction='right' styles={styles} className='pageclip-form__submit' type='submit' />
+      </form>
+    </section>
+  )
+}
