1c0b7984bb84fb09c47dddabdd0418baf2cb8c63
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-27 17:55:45 -0500

    set up individual animations

--- exports/mainBlock.tsx
+++ exports/mainBlock.tsx
@@ -6,57 +6,105 @@ import 'animate.css/animate.min.css'
 import { AnimatedImage, ArticlesGrid, ArticlesSlider, Cities, CompanyMerger, ContactForm, ContentColumns, ContentTabs, FeaturedArticles, FilteredImagesSlider, Gallery, Hero, HeroSlider, Highlights, ImageBar, Images, KnowledgeBase, Leadership, Map, Posts, RecentArticles, RichText, Slideshow, StaticImage, Testimonial } from '../main'
 
 export default (block, i, images, styles, articles, pages, tag, toggleForm) => (
-  <ScrollAnimation animateIn='fadeIn' scrollableParentSelector='#main'>
-    {block.template === 'main-animated-image' ? (
+  block.template === 'main-animated-image' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <AnimatedImage key={i} block={block} images={images} styles={styles} />
-    ) : block.template === 'main-articles-grid' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-articles-grid' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ArticlesGrid key={i} block={block} images={images} styles={styles} articles={articles} pages={pages} tag={tag} />
-    ) : block.template === 'main-articles-slider' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-articles-slider' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ArticlesSlider key={i} block={block} styles={styles} />
-    ) : block.template === 'main-cities' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-cities' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Cities key={i} block={block} images={images} styles={styles} toggleForm={toggleForm} />
-    ) : block.template === 'main-company-merger' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-company-merger' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <CompanyMerger key={i} block={block} images={images} styles={styles} />
-    ) : block.template === 'main-contact-form' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-contact-form' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ContactForm key={i} block={block} styles={styles} />
-    ) : block.template === 'main-content-columns' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-content-columns' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ContentColumns key={i} block={block} styles={styles} images={images} />
-    ) : block.template === 'main-content-tabs' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-content-tabs' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ContentTabs key={i} block={block} styles={styles} images={images} />
-    ) : block.template === 'main-featured-articles' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-featured-articles' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <FeaturedArticles key={i} block={block} images={images} styles={styles} articles={articles} pages={pages} />
-    ) : block.template === 'main-filtered-images-slider' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-filtered-images-slider' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <FilteredImagesSlider key={i} block={block} styles={styles} />
-    ) : block.template === 'main-gallery' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-gallery' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Gallery key={i} block={block} styles={styles} />
-    ) : block.template === 'main-hero' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-hero' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Hero key={i} block={block} images={images} styles={styles} />
-    ) : block.template === 'main-hero-slider' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-hero-slider' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <HeroSlider key={i} block={block} styles={styles} images={images} />
-    ) : block.template === 'main-highlights' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-highlights' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Highlights key={i} block={block} images={images} styles={styles} toggleForm={toggleForm} />
-    ) : block.template === 'main-image-bar' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-image-bar' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <ImageBar key={i} block={block} styles={styles} images={images} />
-    ) : block.template === 'main-images' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-images' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Images key={i} block={block} styles={styles} />
-    ) : block.template === 'main-knowledge-base' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-knowledge-base' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <KnowledgeBase key={i} block={block} styles={styles} />
-    ) : block.template === 'main-leadership' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-leadership' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Leadership key={i} block={block} images={images} styles={styles} />
-    ) : block.template === 'main-map' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-map' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Map key={i} block={block} styles={styles} />
-    ) : block.template === 'main-posts' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-posts' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Posts key={i} block={block} styles={styles} />
-    ) : block.template === 'main-recent-articles' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-recent-articles' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <RecentArticles key={i} block={block} styles={styles} />
-    ) : block.template === 'main-rich-text' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-rich-text' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <RichText key={i} block={block} styles={styles} />
-    ) : block.template === 'main-slideshow' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-slideshow' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150} initiallyVisible>
       <Slideshow key={i} block={block} styles={styles} />
-    ) : block.template === 'main-static-image' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-static-image' ? (
+    <ScrollAnimation animateIn='animate__fadeIn' animateOnce scrollableParentSelector='#main' offset={150}>
       <StaticImage key={i} block={block} images={images} styles={styles} pages={pages} toggleForm={toggleForm} />
-    ) : block.template === 'main-testimonial' ? (
+    </ScrollAnimation>
+  ) : block.template === 'main-testimonial' ? (
+    <ScrollAnimation animateIn='animate__zoomInUp' animateOnce scrollableParentSelector='#main' offset={150}>
       <Testimonial key={i} block={block} styles={styles} images={images} />
-    ) : <p key={i}>{block.template} not defined</p>}
-  </ScrollAnimation>
+    </ScrollAnimation>
+  ) : <p key={i}>{block.template} not defined</p>
 )
