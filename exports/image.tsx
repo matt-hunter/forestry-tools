ba5e4fc63159380af405375e416938b426121caa
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-27 15:24:17 -0500

    adding function support for images

--- exports/image.tsx
+++ exports/image.tsx
@@ -10,23 +10,24 @@ type imageProps = {
   title?: string,
   style?: any,
   children?: React.ReactNode,
+  onClick?: any,
   images: any
 }
 
-export default ({ className, container, src, alt = '', title = '', style, children, images }: imageProps) => {
+export default ({ className, container, src, alt = '', title = '', style, children, images, onClick }: imageProps) => {
   const image = images.find(image => image.relativePath === 'images/' + src.split('/').pop())
 
   return !container ? (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <Img className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title} />
+      <Img onClick={onClick || ''} className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title} />
     ) : image && image.publicURL ? (
-      <img className={className} style={style} src={image.publicURL} alt={alt || title} title={title || alt} />
+      <img onClick={onClick || ''} className={className} style={style} src={image.publicURL} alt={alt || title} title={title || alt} />
     ) : null
   ) : (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <BackgroundImage className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title}>{children}</BackgroundImage>
+      <BackgroundImage onClick={onClick || ''} className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title}>{children}</BackgroundImage>
     ) : image && image.publicURL ? (
-      <div className={className} style={{ ...style, backgroundImage: `url("${image.publicURL}")` }}>{children}</div>
+      <div onClick={onClick || ''} className={className} style={{ ...style, backgroundImage: `url("${image.publicURL}")` }}>{children}</div>
     ) : null
   )
 }
