b20b5b9ab8fee14b709c6a9f67ff909dcc4c6f0e
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-27 00:16:30 -0500

    phone number anchor

--- footer/contactInfo.tsx
+++ footer/contactInfo.tsx
@@ -2,23 +2,21 @@ import React from 'react'
 
 import { Button } from '../shared'
 
-export const ContactInfo = ({ styles, contactInfo, toggleForm }) => {
-  return (
-    <section className={styles.default}>
-      <div className={styles.container}>
-        {contactInfo.phoneNumber && (
-          <p className={styles.phoneNumber}>
-            {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
-          </p>
-        )}
-        <Button text='Get in touch' direction='right' styles={styles} onClick={toggleForm} />
-        {contactInfo.email && (
-          <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
-        )}
-        {contactInfo.address && (
-          <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
-        )}
-      </div>
-    </section>
-  )
-}
+export const ContactInfo = ({ styles, contactInfo, toggleForm }) => (
+  <section className={styles.default}>
+    <div className={styles.container}>
+      {contactInfo.phoneNumber && (
+        <a className={styles.phoneNumber} href={`tel:+1${contactInfo.phoneNumber}`}>
+          {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
+        </a>
+      )}
+      <Button text='Get in touch' direction='right' styles={styles} onClick={toggleForm} />
+      {contactInfo.email && (
+        <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
+      )}
+      {contactInfo.address && (
+        <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
+      )}
+    </div>
+  </section>
+)
--- header/contactInfo.tsx
+++ header/contactInfo.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
 
-export const ContactInfo = ({ styles, contactInfo }) => {
-  return (
-    <section className={styles.default}>
-      <div className={styles.redBar} />
-      <div className={styles.contactInfo}>
-        {contactInfo.phoneNumber && (
-          <p className={styles.phoneNumber}>
-            {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
-          </p>
-        )}
-        {contactInfo.email && (
-          <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
-        )}
-        {contactInfo.address && (
-          <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
-        )}
-      </div>
-    </section>
-  )
-}
+export const ContactInfo = ({ styles, contactInfo }) => (
+  <section className={styles.default}>
+    <div className={styles.redBar} />
+    <div className={styles.contactInfo}>
+      {contactInfo.phoneNumber && (
+        <a className={styles.phoneNumber} href={`tel:+1${contactInfo.phoneNumber}`}>
+          {`+1.${contactInfo.phoneNumber.substr(0, 3)}.${contactInfo.phoneNumber.substr(3, 3)}.${contactInfo.phoneNumber.substr(6, 4)}`}
+        </a>
+      )}
+      {contactInfo.email && (
+        <a className={styles.email} href={'mailto:' + contactInfo.email}>{contactInfo.email}</a>
+      )}
+      {contactInfo.address && (
+        <p className={styles.address} dangerouslySetInnerHTML={{ __html: contactInfo.address.split('\n').join('<br />') }} />
+      )}
+    </div>
+  </section>
+)
