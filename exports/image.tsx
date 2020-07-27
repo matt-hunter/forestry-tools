f44b02220e80309a7dea4d3f39cff6e432e1209b
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-27 16:56:06 -0500

    removed image fade-in

--- exports/image.tsx
+++ exports/image.tsx
@@ -11,22 +11,21 @@ type imageProps = {
   style?: any,
   children?: React.ReactNode,
   onClick?: any,
-  color?: string,
   images: any
 }
 
-export default ({ className, container, src, color, alt = '', title = '', style, children, images, onClick }: imageProps) => {
+export default ({ className, container, src, alt = '', title = '', style, children, images, onClick }: imageProps) => {
   const image = images.find(image => image.relativePath === 'images/' + src.split('/').pop())
 
   return !container ? (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <Img onClick={onClick || ''} backgroundColor={color} className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title} />
+      <Img fadeIn={false} onClick={onClick || ''} className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title} />
     ) : image && image.publicURL ? (
       <img onClick={onClick || ''} className={className} style={style} src={image.publicURL} alt={alt || title} title={title || alt} />
     ) : null
   ) : (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <BackgroundImage backgroundColor={color} onClick={onClick || ''} className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title}>{children}</BackgroundImage>
+      <BackgroundImage fadeIn={false} onClick={onClick || ''} className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title}>{children}</BackgroundImage>
     ) : image && image.publicURL ? (
       <div onClick={onClick || ''} className={className} style={{ ...style, backgroundImage: `url("${image.publicURL}")` }}>{children}</div>
     ) : null
