8770ac9f701d37f73e6cfc3653237f860ceb2c04
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-28 12:15:26 -0500

    static image animations

--- main/staticImage.tsx
+++ main/staticImage.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
+import ScrollAnimation from 'react-animate-on-scroll'
+
+import 'animate.css/animate.min.css'
 
 import { classNames, Image } from '../exports'
 import { Button } from '../shared'
@@ -11,13 +14,19 @@ export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
       <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />
       <div className={styles.textContainer}>
         {block.heading && (
-          <h1 className={styles.heading}>{block.heading}</h1>
+          <ScrollAnimation animateIn='animate__fadeInUp' animateOnce scrollableParentSelector='#main' offset={150} delay={1000}>
+            <h1 className={styles.heading}>{block.heading}</h1>
+          </ScrollAnimation>
         )}
         {block.subheading && (
-          <h2 className={styles.subheading}>{block.subheading}</h2>
+          <ScrollAnimation animateIn='animate__fadeInUp' animateOnce scrollableParentSelector='#main' offset={150} delay={1500}>
+            <h2 className={styles.subheading}>{block.subheading}</h2>
+          </ScrollAnimation>
         )}
         {block.button && block.showButton && (
-          <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
+          <ScrollAnimation animateIn='animate__fadeInUp' animateOnce scrollableParentSelector='#main' offset={150} delay={2000}>
+            <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
+          </ScrollAnimation>
         )}
       </div>
     </section>
