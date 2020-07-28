dcd2ba4d7026f9d1c45ec5489e4b93fdab29f086
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-27 22:45:38 -0500

    leadership mobile detail

--- main/leadership.tsx
+++ main/leadership.tsx
@@ -3,26 +3,15 @@ import React, { useState } from 'react'
 import { classNames, enabled, Image } from '../exports'
 
 export const Leadership = ({ block, styles, images }) => {
-  const [index, setIndex] = useState(0)
-  return (
-    <section className={classNames(block, styles)}>
-      <h1 className={styles.heading}>{block.heading}</h1>
+  const [desktopIndex, setDesktopIndex] = useState(0)
+  const [mobileIndex, setMobileIndex] = useState(undefined)
+
+  const Detail = ({ className, index, setIndex, required }) => (
+    <div className={styles[className]}>
       <div className={styles.leaders}>
-        {enabled(block.leaders).map((leader, i) => {
-          const setActiveIndex = () => {
-            setIndex(i)
-          }
-          return (
-            <div key={i} className={styles.leader + `${index === i ? ` ${styles.active}` : ''}`} onClick={setActiveIndex}>
-              <Image className={styles.image} images={images} container='div' src={leader.photo.image} alt={leader.photo.alt} title={leader.photo.title}>
-                <p className={styles.name}>{leader.name}</p>
-                <p className={styles.title}>{leader.title}</p>
-              </Image>
-            </div>
-          )
-        })}
+        {enabled(block.leaders).map((leader, i) => <Leader key={i} leader={leader} i={i} index={index} setIndex={setIndex} required={required} />)}
       </div>
-      {block.leaders[index] && (
+      {index !== undefined && block.leaders[index] && (
         <div className={styles.detail}>
           {block.leaders[index].name && <h2 className={styles.name}>{block.leaders[index].name}</h2>}
           {block.leaders[index].title && <h3 className={styles.title}>{block.leaders[index].title}</h3>}
@@ -35,6 +24,29 @@ export const Leadership = ({ block, styles, images }) => {
           )}
         </div>
       )}
+    </div>
+  )
+
+  const Leader = ({ leader, i, index, setIndex, required }) => {
+    const setActiveIndex = () => {
+      setIndex(!required && index === i ? undefined : i)
+    }
+
+    return leader && i !== undefined ? (
+      <div className={styles.leader + `${index === i ? ` ${styles.active}` : ''}`} onClick={setActiveIndex}>
+        <Image className={styles.image} images={images} container='div' src={leader.photo.image} alt={leader.photo.alt} title={leader.photo.title}>
+          <p className={styles.name}>{leader.name}</p>
+          <p className={styles.title}>{leader.title}</p>
+        </Image>
+      </div>
+    ) : null
+  }
+
+  return (
+    <section className={classNames(block, styles)}>
+      <h1 className={styles.heading}>{block.heading}</h1>
+      <Detail className='mobile' index={mobileIndex} setIndex={setMobileIndex} required={false} />
+      <Detail className='desktop' index={desktopIndex} setIndex={setDesktopIndex} required />
     </section>
   )
 }
