a723c23122c6a5694c75a4bad88f4947eaef2826
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-29 13:42:04 -0500

    fix for empty video array

--- main/staticImage.tsx
+++ main/staticImage.tsx
@@ -9,10 +9,11 @@ export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
     const video = images.find(video => video.relativePath === 'images/' + videoFile.split('/').pop())
     return video && video.publicURL
   })
+  console.log(videos)
 
   return (
     <section className={classNames(block, styles)}>
-      {block.image && !videos && <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />}
+      {block.image && !videos.length && <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />}
       <div className={styles.textContainer}>
         {block.heading && (
           <h1 className={styles.heading}>{block.heading}</h1>
@@ -24,11 +25,11 @@ export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
           <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
         )}
       </div>
-      {videos && (
+      {videos && videos.length ? (
         <div className={styles.videoContainer}>
           <ReactPlayer autoplay playing muted loop playsinline controls={false} volume={0} pip={false} className={styles.video} url={videos} width='100%' height='100%' />
         </div>
-      )}
+      ) : null}
     </section>
   )
 }
