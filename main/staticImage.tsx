daa292f4f63b596aa8dc1dd620047132e9174f59
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-07-29 10:52:22 -0500

    initial video support

--- main/staticImage.tsx
+++ main/staticImage.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ReactPlayer from 'react-player/lazy'
 
 import { classNames, Image } from '../exports'
 import { Button } from '../shared'
 
 export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
+  const Video = () => {
+    const videos = block.videos && block.videos.map(videoFile => {
+      const video = images.find(video => video.relativePath === 'images/' + videoFile.split('/').pop())
+      return video && video.publicURL
+    })
+
+    return <ReactPlayer autoplay playing muted loop controls={false} volume={0} pip={false} className={styles.video} url={videos} width='100%' height='100%' playsinline />
+  }
+
   return (
     <section className={classNames(block, styles)}>
       <Image images={images} className={styles.image} container='div' src={block.image.image} title={block.image.title} alt={block.image.alt} />
@@ -18,6 +28,7 @@ export const StaticImage = ({ block, styles, images, pages, toggleForm }) => {
           <Button text='Find out more' direction='right' styles={styles} pages={pages} link={block.button.link} onClick={toggleForm} />
         )}
       </div>
+      <Video />
     </section>
   )
 }
