3c0f0628efa29daf76ae99135c2ee6c58a6ee836
Matt Hunter <matt@Matts-MacBook-Pro.local>
2020-08-04 16:38:43 -0500

    add fallback titles

--- exports/image.tsx
+++ exports/image.tsx
@@ -19,13 +19,13 @@ export default ({ className, container, src, alt = '', title = '', style, childr
 
   return !container ? (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <Img fadeIn={false} onClick={onClick || ''} className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title} />
+      <Img fadeIn={false} onClick={onClick || ''} className={className} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title || 'no title'} />
     ) : image && image.publicURL ? (
       <img onClick={onClick || ''} className={className} style={style} src={image.publicURL} alt={alt || title} title={title || alt} />
     ) : null
   ) : (
     image && image.childImageSharp && image.childImageSharp.fluid ? (
-      <BackgroundImage fadeIn={false} onClick={onClick || ''} className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title}>{children}</BackgroundImage>
+      <BackgroundImage fadeIn={false} onClick={onClick || ''} className={className} Tag={container || 'div'} style={style} fluid={image.childImageSharp.fluid} title={title} alt={alt || title || 'no title'}>{children}</BackgroundImage>
     ) : image && image.publicURL ? (
       <div onClick={onClick || ''} className={className} style={{ ...style, backgroundImage: `url("${image.publicURL}")` }}>{children}</div>
     ) : null
